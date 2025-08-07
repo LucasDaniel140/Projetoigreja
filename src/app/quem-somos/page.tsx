5e1cdab0eea0f95220ba60580b30e65c16eb08c6
Lucas Daniel <lucasdaniel.lem@gmail.com>
2025-08-07 14:03:13 +0000

    não esta automatica!!!!!!!!

--- src/app/quem-somos/page.tsx
+++ src/app/quem-somos/page.tsx
@@ -165,7 +165,6 @@ export default function QuemSomosPage() {
                   </CarouselItem>
                 ))}
               </CarouselContent>
-              
             </Carousel>
         </section>
       </div>
