dcc0a70af896dba2b273ce34ad37524b10c37c54
Lucas Daniel <lucasdaniel.lem@gmail.com>
2025-08-07 20:11:14 +0000

    No video do youtube, tem como remover o icone do youtube vermelho

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -228,7 +228,6 @@ export default function Home() {
                                             className="object-cover transition-transform duration-300 group-hover:scale-110"
                                         />
                                         <div className="absolute inset-0 bg-black/40 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-                                            <Youtube className="w-12 h-12 text-white" />
                                         </div>
                                     </div>
                                 </Link>
