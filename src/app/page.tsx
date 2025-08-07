8c8c16dbe7e5c4a5660f7d2fb0d29cb1be79495b
Lucas Daniel <lucasdaniel.lem@gmail.com>
2025-08-07 20:03:05 +0000

    Eu quero dessa forma, o vermelho é o texto, e o verde é o mapa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -178,12 +178,14 @@ export default function Home() {
           </div>
         </div>
       </section>
+      
       <section className="w-full py-12 md:py-24 lg:py-32">
         <div className="container px-4 md:px-6 grid md:grid-cols-2 gap-16 items-start">
-            <div className="h-full w-full rounded-lg overflow-hidden min-h-[350px] md:min-h-[400px]">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7802.747970446119!2d-45.80001752432361!3d-12.086529888153335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x934a713399ab35a1%3A0xf279243579f3a83b!2sMinist%C3%A9rio%20Vivendo%20a%20Palavra!5e0!3m2!1spt-PT!2sus!4v1754596320009!5m2!1spt-PT!2sus"
+            <div className="h-full w-full rounded-lg overflow-hidden">
+                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7802.747970446119!2d-45.80001752432361!3d-12.086529888153335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x934a713399ab35a1%3A0xf279243579f3a83b!2sMinist%C3%A9rio%20Vivendo%20a%20Palavra!5e0!3m2!1spt-PT!2sus!4v1754596320009!5m2!1spt-PT!2sus"
                     width="100%"
                     height="100%"
+                    className="aspect-square"
                     style={{ border: 0 }}
                     allowFullScreen={true}
                     loading="lazy"
